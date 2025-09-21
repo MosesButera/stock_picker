e3e9fc3bca0fccaebc375af455320dd079f3cc4a
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-09-21 08:22:43 -0600

    Initialize tracking variables for the analysis Define variables to track maximum profit, minimum price, and potential buy/sell days. This creates the starting point for comparisons as the loop progresses.

--- index.rb
+++ index.rb
@@ -1,10 +1,15 @@
 
 def stock_picker(prices = nil)
-  
+
   return "please enter a parameter of an array of daily prices" if prices.nil?
   return "Please enter valid array e.g [1,3,4,9]" unless prices.is_a?(Array) 
 
-
+    #Initialize all variables
+    max_profit = -Float::INFINITY
+    min_price = prices[0]
+    current_price = 0
+    buy_price = 0
+    sell_price = 1
 
 
 
